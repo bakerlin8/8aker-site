4fb90f91ca82bbbfd6015a8ce9d75dd7f9a93621
bakerlin8 <nr.bater@gmail.com>
2026-09-20 21:19:45 +0800

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const clientId = process.env.OAUTH_CLIENT_ID;
+  const host = req.headers.host;
+  const redirectUri = `https://${host}/api/callback`;
+  const scope = 'repo,user';
+  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
+  res.writeHead(302, { Location: authUrl });
+  res.end();
+}
